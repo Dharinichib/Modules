2a623b2a5eb3ddffda06bcf44abac12d6c3cec7b
Dharinichib <127197788+Dharinichib@users.noreply.github.com>
2023-03-11 20:00:07 +0530

    Delete Assignment2 directory

--- Assignment2/program3.js
+++
@@ -1,7 +0,0 @@
-function sum(num1, num2){
-    return num1 + num2;
-}
-const prompt = require("prompt-sync")();
-a = parseInt(prompt("Enter first number: "));
-b = parseInt(prompt("Enter second number: "));
-console.log(sum(a,b));
