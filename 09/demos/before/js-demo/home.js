c1e6482c83a0c66d7f49d7b5b422095b71db6f5b
Dharini <dharinichib@gmail.com>
2023-03-14 20:54:28 +0530

    14-03-2023

--- 09/demos/before/js-demo/home.js
+++ 09/demos/before/js-demo/home.js
@@ -1,18 +1,32 @@
-
-const button = document.getElementById('see-review');
-
-button.addEventListener('click', function() {
-
-    const review = document.getElementById('review');
-
-    if (review.classList.contains('d-none')) {
-        review.classList.remove('d-none');
-        button.textContent = 'CLOSE REVIEW';
-    }
-    else {
-        review.classList.add('d-none');
-        button.textContent = 'SEE REVIEW';
-    }
-    
+/*const values = ['a','b','c'];
+console.log(values);
+console.log(Array.isArray(values));
+console.log(values.length);
+values[0]='d';
+console.log(values[0]);
+let a = values.push('e')
+console.log(a);
+let  = values.pop()
+console.log(values);
+const values = ['a','b','c'];
+const newval = values.slice(1,3);
+console.log(newval);
+console.log(values);
+const values = ['a','b','c','dd'];
+//const newval = values.splice(1,1,"f");
+const newval = values.splice(0,1);
+console.log(newval);
+console.log(values);
+console.log(values.indexOf('b'));
+const set = values.filter(function(item){
+    return item >'b';
+});
+console.log(set);
+const found = values.find(function(item){
+    return item.length>1;
 
 });
+console.log(found);*/
+const containers 
+= document.getElementsByClassName('container');
+console.log(containers);
