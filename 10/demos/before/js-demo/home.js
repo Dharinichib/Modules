5f9eea19c2e82056978f702fa9d912872480480a
Dharini <dharinichib@gmail.com>
2023-03-14 20:51:44 +0530

    do

--- 10/demos/before/js-demo/home.js
+++ 10/demos/before/js-demo/home.js
@@ -1,5 +1,29 @@
+/*let productId = 12345;
+function showProductId(){
+    console.log(productId);
+}
 
-const containers = 
-    document.getElementsByClassName('container');
-containers[0].classList.add('d-none');
-console.log(containers);
+const app ={ productId : 1234,
+    username : 'joe',
+    orderNumber : 789};
+function showProductId(){
+    console.log(app.productId);
+}
+showProductId();
+function showProductId(){
+    let productId = 12345;
+    function fix(){
+        let productId = 5678;
+        console.log('infix: ', productId);
+
+    }
+    fix();
+    console.log("in showproductid :", productId);
+}
+showProductId();
+productId = 456;
+console.log(productId);
+let productId = 123;*/
+'use strict';
+productId = 123;
+console.log(productId);
