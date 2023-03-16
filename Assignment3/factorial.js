fffb32a47587e1d76a77002d0c0f4661b2f117c5
Dharinichib <127197788+Dharinichib@users.noreply.github.com>
2023-03-16 19:52:07 +0530

    Delete Assignment3 directory

--- Assignment3/factorial.js
+++
@@ -1,8 +0,0 @@
-function fact(n){
-    if(n == 1){
-        return 1;
-    }
-    return n*fact(n-1);
-}
-let x = fact(1);
-console.log(x);
--- Assignment3/sum_of_digit.js
+++
@@ -1,9 +0,0 @@
-function sum(x){
-    if (x == 0){
-        return 0;
-    }
-    
-    return (x%10 + sum(parseInt(x/10)))
-}
-let x = sum(1234);
-console.log(x);
