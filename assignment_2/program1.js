73b633be3da6d1e008e66662ac05869f1de9d34b
Dharinichib <127197788+Dharinichib@users.noreply.github.com>
2023-03-16 19:52:42 +0530

    Delete assignment_2 directory

--- assignment_2/program1.js
+++
@@ -1,8 +0,0 @@
-function sum(num1){
-    return function(num2)
-    {
-        return num1 + num2;
-    }
-}
-
-console.log(sum(10)(20));
--- assignment_2/program2.js
+++
@@ -1,8 +0,0 @@
-function sum(num1){
-    return function sum1(num2)
-    {
-        return num1 + num2;
-    }
-}
-let a = sum(10);
-console.log(a(20));
