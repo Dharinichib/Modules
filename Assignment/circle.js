03a0ee8bb845b01f2698da099a6a219f0b145fd9
Dharinichib <127197788+Dharinichib@users.noreply.github.com>
2023-03-16 19:51:51 +0530

    Delete Assignment directory

--- Assignment/circle.js
+++
@@ -1,6 +0,0 @@
-const prompt = require("prompt-sync")();
-var radius = prompt("enter Radius ");
-var area =  (22/7)*radius;
-var perimeter = 2*(22/7)*radius;
-console.log("Area of Circle ",area);
-console.log("Perimeter of Circle ",perimeter);
--- Assignment/swap.js
+++
@@ -1,8 +0,0 @@
-const prompt = require("prompt-sync")();
-var a = prompt("Enter value of a: ");
-var b = prompt("Enter value of b: ");
-var temp;
-temp = a;
-a = b;
-b = temp;
-console.log("After swapping a: "+a +"\n b: " +b);
