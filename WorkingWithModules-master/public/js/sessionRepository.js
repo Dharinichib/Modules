9dfc5309eb0a3b3dd8fd6c7a9b116d0302e38bb4
Dharinichib <127197788+Dharinichib@users.noreply.github.com>
2023-03-29 15:25:49 +0530

    28-03-2023

---
+++ WorkingWithModules-master/public/js/sessionRepository.js
@@ -0,0 +1,22 @@
+//import { sessionTemplate } from "./template";
+
+const sessionURL = 'sessions.json';
+
+let sessionList = [];
+
+ function getSessions(){
+    return new Promise(function(resolve, reject){
+      var oReq = new XMLHttpRequest();
+      oReq.onload = function (e) {
+        sessionList = e.target.response;
+        resolve(sessionList);
+      };
+      oReq.open('GET', sessionURL , true);
+      oReq.responseType = 'json';
+      oReq.send();
+    })
+  }
+
+  export {getSessions as default, sessionURL};
+  export{sessionTemplate}from './template.js'
+ 
